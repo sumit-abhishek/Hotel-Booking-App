9453f18ed343806775653b9d27ca1e9e0ecd19d4
sumit-abhishek <sumitabhi1997@gmail.com>
2024-08-07 15:49:02 +0530

    Adding Media Query in Home Page

--- js/index.js
+++ js/index.js
@@ -50,6 +50,7 @@ if (loginUserData) {
 
     const { searchLocation, checkInDate, checkOutDate, numberGuest } =
       searchLoaction.elements;
+    //Check for Correct Input Date
     let dateObj = new Date();
     let month = String(dateObj.getMonth() + 1).padStart(2, "0");
     let day = String(dateObj.getDate()).padStart(2, "0");
